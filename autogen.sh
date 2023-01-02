90b77380afab79e8b5bbd6e5643953180764c7fe
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 21:16:29 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,3 @@
-#!/bin/sh
-# SPDX-License-Identifier: GPL-2.0+
-
-aclocal && \
-autoheader && \
-autoconf && \
-libtoolize && \
-automake -a -c
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:78b774b7e735252a201c4ed9eb2bcd219536886fd8ef24b57801971a5483dc7d
+size 122
